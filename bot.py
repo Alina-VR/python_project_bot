966e4cc71d60ec8fbf0f9104610b308eb152f487
Alina-VR <raznosilina.av@phystech.edu>
2023-11-26 23:37:04 +0300

    feature: bot can get events from database, add /help; feat: tests

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+import datetime
+
 import telebot
 import config
-from bot_db import BotDB
+from bot_db import BotDB, DELTA_UTC
 
 bot_db = BotDB('database.db')
 bot = telebot.TeleBot(config.TOKEN)
@@ -10,10 +12,11 @@ bot = telebot.TeleBot(config.TOKEN)
 def welcome(message):
     if not bot_db.is_user_in_db(message.from_user.id):
         bot_db.add_user(message.from_user.id)
-    sticker = open('sticker.tgs', 'rb')
+    sticker = open('hello-sticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, sticker)
     bot.send_message(message.chat.id, 'Добро пожаловать, {0.first_name}!\nЯ - {1.first_name}, '
-                                      'помогу тебе распланировать время и не забыть ничего важного.'.
+                                      'помогу тебе распланировать время и не забыть ничего важного. Чтобы узнать, что '
+                                      'я могу - пиши /help.'.
                      format(message.from_user, bot.get_me()))
 
 
@@ -29,9 +32,59 @@ def add_event_with_feedback(message):
     bot.send_message(message.chat.id, 'Отлично, событие записано!')
 
 
+@bot.message_handler(commands=['today_events'])
+def show_events(message):
+    records = bot_db.get_events(message.from_user.id, 'day')
+    if len(records):
+        answer = 'Все события за день \n'
+        for r in records:
+            answer += 'Событие: ' + r[2] + ', '
+            answer += 'Время: ' + str(datetime.datetime.fromisoformat(r[3]) + DELTA_UTC) + '\n'
+        bot.send_message(message.chat.id, answer)
+    else:
+        bot.send_message(message.chat.id, 'Ещё нет событий. Давай что-нибудь запишем?')
+
+
+@bot.message_handler(commands=['month_events'])
+def show_events(message):
+    records = bot_db.get_events(message.from_user.id, 'month')
+    if len(records):
+        answer = 'Все события за месяц \n'
+        for r in records:
+            answer += 'Событие: ' + r[2] + ', '
+            answer += 'Время: ' + str(datetime.datetime.fromisoformat(r[3]) + DELTA_UTC) + '\n'
+        bot.send_message(message.chat.id, answer)
+    else:
+        bot.send_message(message.chat.id, 'Ещё нет событий. Давай что-нибудь запишем?')
+
+
+@bot.message_handler(commands=['all_events'])
+def show_events(message):
+    records = bot_db.get_events(message.from_user.id, 'all')
+    if len(records):
+        answer = 'Все события \n'
+        for r in records:
+            answer += 'Событие: ' + r[2] + ', '
+            answer += 'Время: ' + str(datetime.datetime.fromisoformat(r[3]) + DELTA_UTC) + '\n'
+        bot.send_message(message.chat.id, answer)
+    else:
+        bot.send_message(message.chat.id, 'Ещё нет событий. Давай что-нибудь запишем?')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Давай я расскажу тебе, что я могу😊\nКак только ты напишешь мне /start - я '
+                                      'запомню тебя, и смогу запоминать также твои события.\nПиши /record_event чтобы '
+                                      'записать событие.\n Если хочешь вспомнить свои события за день - '
+                                      'пиши /today_events, за месяц - /month_events, за всё время - /all_events.\n '
+                                      'Если не помнишь, как мне написать - набери /help и увидишь все мои команды.')
+
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
-    bot.send_message(message.chat.id, 'Извини, пока не понимаю(')
+    bot.send_message(message.chat.id, 'Извини, пока не понимаю')
+    sticker = open('sorry_sticker.tgs', 'rb')
+    bot.send_sticker(message.chat.id, sticker)
 
 
 bot.polling(none_stop=True)
--- bot_db.py
+++ bot_db.py
@@ -1,6 +1,8 @@
 import sqlite3
 import datetime
 
+DELTA_UTC = datetime.timedelta(hours=3)
+
 
 class BotDB:
     def __init__(self, db_file):
@@ -25,10 +27,27 @@ class BotDB:
         user_id = message.from_user.id
         text = message.text.split()
         event = ' '.join(text[:-5])
-        event_time = datetime.datetime(int(text[-5]), int(text[-4]), int(text[-3]), int(text[-2]), int(text[-1]))
+        event_time = datetime.datetime(int(text[-5]), int(text[-4]), int(text[-3]), int(text[-2]),
+                                       int(text[-1])) - DELTA_UTC
         self.cursor.execute('INSERT INTO "records" ("user_id", "event", "event_time") VALUES (?,?,?)',
                             (self.get_user_id(user_id), event, event_time))
         self.connection.commit()
+
+    def get_events(self, user_id, within):
+        if within == 'day':
+            result = self.cursor.execute('SELECT * FROM "records" WHERE "user_id" = ?  AND "event_time" '
+                                         "BETWEEN datetime('now') AND datetime('now','start of day', '1 days', '-3 hours') order by 'event_time'",
+                                         (self.get_user_id(user_id),))
+            self.connection.commit()
+        elif within == 'month':
+            result = self.cursor.execute('SELECT * FROM "records" WHERE "user_id" = ?  AND "event_time" '
+                                         "BETWEEN datetime('now') AND datetime('now','start of month', '1 months', '-3 hours') order by 'event_time'",
+                                         (self.get_user_id(user_id),))
+            self.connection.commit()
+        else:
+            result = self.cursor.execute('SELECT * FROM "records" WHERE "user_id" = ? order by "event_time"', (self.get_user_id(user_id),))
+            self.connection.commit()
+        return result.fetchall()
 
     def close(self):
         self.connection.close()
